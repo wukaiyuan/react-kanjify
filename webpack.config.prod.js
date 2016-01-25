ea814d2e2608cafc8f6e10f1ea7ba5e44b3b7565
Brian Eng <wukaiyuan@gmail.com>
2016-01-25 00:41:31 -0800

    Chrome extension works

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -29,6 +29,10 @@ module.exports = {
       test: /\.js$/,
       loaders: ['babel'],
       include: path.join(__dirname, 'src')
+    },
+    {
+      test: /\.css$/,
+      loader: "style!css"
     }]
   }
 };
